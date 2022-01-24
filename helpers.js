5b5befa595566ac1585da89f90799e11170d6b9b
MayankArya123 <mayanksunny786@gmail.com>
2022-01-24 11:46:31 +0530

    two-apis-added

---
+++ helpers.js
@@ -0,0 +1,74 @@
+const axios = require("axios")
+
+
+const GetNearByTouristPlaces = async (latitude, longitude, placename, cb) => {
+  console.log(latitude)
+  console.log(longitude)
+
+  const response = await axios.get(
+    `https://api.tomtom.com/search/2/poiSearch/tourist attraction.json?key=utQHsOJFx99x4BwG8NUuAa3QQ1MR5FBG&=&limit=10&lat=${latitude}&lon=${longitude}&radius=50000`
+  )
+
+  if (response.data.summary) {
+    console.log("seeing response    jjjjjjjjjjjjjjjjj", response.data.summary)
+    response.data.summary.placeName = placename
+    delete response.data.summary.queryType
+    // const { ,...rest } = response.data
+    console.log(response.data.summary)
+
+    const DistanceResponse = await axios.get(
+        `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=22.95456,76.042034&destinations=22.96974,76.06057&departure_time=now&key=b0VzzHpdfJzjGuQukhVIGv5ci6gkZ`
+      )
+   
+    //   console.log('distance response',DistanceResponse.data.rows[0].elements[0].distance.text)
+    //   console.log('distance response',DistanceResponse.data.rows[0].elements[0].duration.text)
+
+    //   response.data.summary.distance = DistanceResponse.data.rows[0].elements[0].distance.text
+    //   response.data.summary.duration = DistanceResponse.data.rows[0].elements[0].duration.text
+
+    cb(undefined, response)
+  } else {
+    // console.log('dddddd',response);
+    cb("something went wrong")
+  }
+}
+
+const GeoCode = (address, cb) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoibWF5YW5rMTI0IiwiYSI6ImNreTlwdXBldjA4aGoyb29jbjd5ZW16cnAifQ.7InN2qkKmpdcSiHYknzJkQ"
+
+  axios
+    .get(url)
+    .then(({data}) => {
+      if (data.features.length > 0) {
+        console.log(
+          "response",
+          data.features[0].center,
+          data.features[0].place_name
+        )
+        const {center, place_name} = data.features[0]
+        cb(undefined, center, place_name)
+      } else {
+        console.log("unable to find location")
+        cb("unable to find location")
+      }
+    })
+    .catch((err) => {
+      if (err) {
+        if (err) {
+          if (err.response) {
+            console.log(err.response.data.message)
+            cb(err.response.data.message)
+          } else {
+            console.log("network -error", err)
+            cb("network error")
+          }
+        }
+      }
+    })
+}
+
+
+module.exports ={GeoCode,GetNearByTouristPlaces}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const axios = require("axios")
-const cors = require('cors')
-const path = require('path');
+const cors = require("cors")
+const path = require("path")
 
-const {MONGO_URI,PORT} = require('./Config/keys')
+const {GeoCode, GetNearByTouristPlaces} = require("./helpers")
+
+const {MONGO_URI, PORT} = require("./Config/keys")
 
 const app = express()
 
@@ -13,22 +15,58 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/GetNearByTouristPlaces", async (req, res) => {
-  console.log('route hitting',req.query);
+  console.log("route hitting", req.query)
 
   const {lat, long} = req.query
 
   const latitude = lat
 
-  const longitude =  long
+  const longitude = long
 
-  console.log(latitude,typeof(latitude));
+  console.log(latitude, typeof latitude)
 
   try {
     const response = await axios.get(
-      `https://api.tomtom.com/search/2/poiSearch/tourist attraction.json?key=utQHsOJFx99x4BwG8NUuAa3QQ1MR5FBG&=&limit=10&lat=${latitude}&lon=${longitude}&radius=10000`
+      `https://api.tomtom.com/search/2/poiSearch/tourist attraction.json?key=utQHsOJFx99x4BwG8NUuAa3QQ1MR5FBG&=&limit=10&lat=${latitude}&lon=${longitude}&radius=50000`
     )
-    console.log('seeing response',response.data);
+
+
+    console.log("seeing response", response.data)
+    
     res.send(response.data)
+  } catch (err) {
+    console.log("error ", err)
+    res.status(400).send()
+  }
+})
+
+app.get("/nearByTouristPlacesOfPerticularAddress", async (req, res) => {
+  console.log("route hitting by static address", req.query)
+
+  const {address} = req.query
+
+  console.log(address)
+
+  try {
+    GeoCode(address, (err, coordinates, place_name) => {
+      if (err) {
+        return res.send(400).send({
+          error: err,
+        })
+      }
+      console.log("coordinates", coordinates, place_name)
+
+      const latitude = coordinates[1]
+      const longitude = coordinates[0]
+
+      GetNearByTouristPlaces(latitude, longitude, place_name, (err, data) => {
+        if (err) {
+          return res.send({error: err})
+        }
+
+        res.send(data.data)
+      })
+    })
   } catch (err) {
     console.log("error ", err)
     res.status(400).send()
@@ -36,20 +74,20 @@ app.get("/GetNearByTouristPlaces", async (req, res) => {
 })
 
 app.get("/GetTouristPlaceDetails", async (req, res) => {
+  console.log("route hitting 1", req.params)
 
-  console.log('route hitting 1',req.params);
+  //   const {lat, long} = req.query
 
-//   const {lat, long} = req.query
+  const id = "356008000000071"
 
-  const id = '356008000000071'
-
-//   const longitude = long
+  //   const longitude = long
 
   try {
     const response = await axios.get(
       `https://api.tomtom.com/search/2/place.json?entityId=${id}&key=utQHsOJFx99x4BwG8NUuAa3QQ1MR5FBG&language=NGT`
     )
-    console.log('seeing response',response.data);
+    console.log("seeing response", response.data)
+    delete response.data.summary.queryType
     res.send(response.data)
   } catch (err) {
     console.log("error ", err)
@@ -57,16 +95,12 @@ app.get("/GetTouristPlaceDetails", async (req, res) => {
   }
 })
 
-  
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
 
-  app.use(express.static('client/build'))
-
-  app.get('*', (req,res)=> {
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
 }
-
-
 
 app.listen(port, () => console.log("server listening at port", PORT))
